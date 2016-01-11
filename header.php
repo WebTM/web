42f007eac6884a57daf4559e97c6e8e502d18b5a
fullgod <fullgod@gmail.com>
2016-01-11 16:19:08 +0300

    123

--- header.php
+++ header.php
@@ -15,9 +15,9 @@
 
 <body>
 <?php 
-
+	get_template_part('menu/menu','nav-start'); 
 	get_template_part('menu/menu', web_get_header_layout()); 
-
+	get_template_part('menu/menu','nav-start'); 
 		
 ?>
 <div id="page">
--- page_page_builder.php
+++ page_page_builder.php
@@ -3,7 +3,7 @@
 	Template Name: Builder
 	*/
 	get_header();
-	//the_post(); 
-	//the_content();
+	the_post(); 
+	the_content();
 	get_footer();
 	
