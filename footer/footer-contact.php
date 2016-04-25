1cff144ec9bd137cfa8219943f639f6b766edfd2
fullgod <fullgod@gmail.com>
2016-04-25 19:06:47 +0300

    123

--- footer/footer-contact.php
+++ footer/footer-contact.php
@@ -3,10 +3,7 @@
     <div class="bg-right trans-slow"></div>
 	<div class="container">
 		<div class="row">
-            
-            
-			<div class="col-sm-12">
-                
+			<div class="col-sm-12">           
 				<?php
 					/**
 					 * Subfooter nav menu, allows top level items only
@@ -23,32 +20,13 @@
 					    );
 					}
 				?>
+			    <div class="menu-socials">
+					<a href="<?php  home_url(); ?>" class="text-white"><i class="icon social_skype"></i></a>
+      				<a href="<?php  home_url(); ?>" class="text-white"><i class="icon social_wordpress_square"></i></a>
+       				<a href="<?php  home_url(); ?>" class="text-white"><i class="icon icon_mobile"></i></a>
+      	  			<a href="<?php  home_url(); ?>" class="text-white"><i class="icon social_instagram_square"></i></a>
+    			</div>
 			</div>
-            
-            
-    <?php if( get_option('cta_footer_url', home_url()) ) : ?>
-		<div class="contact-action">
-			<div class="align-vertical">
-				<a href="<?php echo esc_url( get_option('cta_footer_url', home_url()) ); ?>" class="text-white">
-					<span class="text-white"><?php echo get_option('cta_footer_text', 'Get in touch with us'); ?> <i class="icon arrow_right"></i></span>
-				</a>
-			</div>
-		</div>
-	<?php endif; ?>
-    
-    
-	<div class="site-info">
-			&copy; 
-            <?php echo date("Y") ?>
-            <?php bloginfo( 'name' ); ?>
-    </div>
-    <div class="menu-socials">
-		<a href="<?php  home_url(); ?>" class="text-white"><i class="icon social_skype"></i></a>
-        <a href="<?php  home_url(); ?>" class="text-white"><i class="icon social_wordpress_square"></i></a>
-        <a href="<?php  home_url(); ?>" class="text-white"><i class="icon icon_mobile"></i></a>
-        <a href="<?php  home_url(); ?>" class="text-white"><i class="icon social_instagram_square"></i></a>
-    </div>
-    
 		</div><!--end for row-->
 	</div><!--end of container-->
 </footer>
