c0d4f7dbc7547ea1ee1c3aea3e7339a3dd05c134
fullgod <fullgod@gmail.com>
2016-06-28 17:04:29 +0300

    123

--- header/content-header-logo.php
+++ header/content-header-logo.php
@@ -4,23 +4,23 @@
 	if(is_array($attachments)) :
 ?>
 
-<header class="page-header ebor-pad-me">
+<header class="page-header">
 
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12 text-center">
+			<div class="col-lg-12">
 				<?php 
 					if( isset( $attachments[0] ) )
 						echo wp_get_attachment_image($attachments[0], 'full', false, array('class' => 'logo'));
 						
 					if( $small )
-						echo '<span class="text-white alt-font">'. htmlspecialchars_decode($small) .'</span>';
+						echo '<span class="alt-font">'. htmlspecialchars_decode($small) .'</span>';
 					
 					if( $big )
-						echo '<h1 class="text-white space-bottom-medium">'. htmlspecialchars_decode($big) .'</h1>';
+						echo '<h1 class="space-bottom-medium">'. htmlspecialchars_decode($big) .'</h1>';
 						
 					if( $sub )	
-						echo '<p class="lead text-white">'. htmlspecialchars_decode($sub) .'</p>';
+						echo '<p class="lead">'. htmlspecialchars_decode($sub) .'</p>';
 				
 					if( $shortcode )
 						echo do_shortcode(htmlspecialchars_decode($shortcode));
