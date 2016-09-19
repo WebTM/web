8f29600ba7f8284b57f4a8c46677c4d7ebaf3ff6
fullgod <fullgod@gmail.com>
2016-09-19 16:05:13 +0300

    123

--- template_parts/content-footer-ui.php
+++ template_parts/content-footer-ui.php
@@ -1,12 +1,69 @@
+<section class="vc_row-fluid vc_custom_1471959441278">
+<div class="container">
+	<div class="row">
+		<div class="vc_col-sm-2">
+			<div class="feature feature-icon-large">
+				<a href="http://getbootstrap.com/" target="_blank" class="feature feature-icon-large" data-shortname="bootstrap">
+				<img src="http://svgporn.com/logos/bootstrap.svg" alt="Bootstrap" class="bootstrap">
+				</a>
+			</div>
+		</div>
+		<div class="vc_col-sm-2">
+			<div class="feature feature-icon-large">
+				<a href="http://www.w3.org/TR/CSS/" target="_blank" class="feature feature-icon-large" data-shortname="css-3">
+				<img src="http://svgporn.com/logos/css-3.svg" alt="CSS3" class="css-3">
+				</a>
+			</div>
+		</div> 
+		<div class="vc_col-sm-2">
+			<div class="feature feature-icon-large">
+				<a href="http://sass-lang.com/" target="_blank" class="feature feature-icon-large" data-shortname="sass">
+				<img src="http://svgporn.com/logos/sass.svg" alt="Sass" class="sass"></a>
+			</div>
+		</div> 
+		<div class="vc_col-sm-2">
+			<div class="feature feature-icon-large">
+				<a href="http://www.w3.org/TR/html5/" target="_blank" class="feature feature-icon-large" data-shortname="html-5">
+				<img src="http://svgporn.com/logos/html-5.svg" alt="HTML5" class="html-5"></a>
+			</div>
+		</div>  
+		<div class="vc_col-sm-2">
+			<div class="feature feature-icon-large">
+				<a href="https://wordpress.org/" target="_blank" class="feature feature-icon-large" data-shortname="wordpress">
+				<img src="http://svgporn.com/logos/wordpress-icon.svg" alt="Wordpress" class="wordpress"></a>
+			</div>
+		</div> 
+		<div class="vc_col-sm-2 ">
+			<div class="feature feature-icon-large">
+				<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" class="feature feature-icon-large" data-shortname="javascript">
+				<img src="http://svgporn.com/logos/javascript.svg" alt="Javascript (JS)" class="javascript"></a>
+			</div>
+		</div> 
+
+  	</div>
+</div>
+</section>
+
 <footer class="footer__ui">
 	<div class="container">
 		<div class="row">
+
 			<div class="col-sm-12">           
-			<a class="footer__ui-logo" title="UI" rel="home">Hacking UI</a>          
-			    <div class="footer__ui-text">
-					Веб-разработка на WordPress
-    			</div>
+				<a class="footer__ui-logo" title="UI" rel="home">Hacking UI</a>
 			</div>
-		</div><!--end for row-->
+
+		    <div class="col-lg-3 vcenter">
+        	<div class="footer__ui-copyright alignleft">2016</div>
+    		</div>
+
+	   	 	<div class="col-lg-8 vcenter">
+	        <div class="footer__ui-icons alignright">
+	
+
+				<a href="https://www.linkedin.com/" target="_blank" class="feature" data-shortname="linkedin"><img src="http://svgporn.com/logos/linkedin.svg" alt="LinkedIn" class="linkedin"></a>
+				<a href="https://twitter.com/" target="_blank" class="feature" data-shortname="twitter"><img src="http://svgporn.com/logos/twitter.svg" alt="Twitter" class="twitter"></a>
+	        </div>
+	   		</div>
+
 	</div><!--end of container-->
 </footer>
