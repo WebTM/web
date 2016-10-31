d7097e65b7c39de41e597925a9f6c897ad452872
fullgod <fullgod@gmail.com>
2016-10-31 16:02:35 +0300

    foooter
    https://www.x5.ru/ru/Pages/Investors.aspx

---
+++ template_parts/content-footer-portfolio.php
@@ -0,0 +1,20 @@
+<footer class="footer__ui">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-12">           
+				<a class="footer__ui-logo" title="UI" rel="home">Hacking UI</a>
+			</div>
+
+		    <div class="col-sm-4 col-xs-12">
+        		<div class="footer__ui-copyright alignleft">2016</div>
+    		</div>
+	   	 	
+	   	 	<div class="col-sm-8 col-xs-12">
+	        	<div class="footer__ui-icons alignright">
+					<a href="https://twitter.com/" target="_blank" class="feature" data-shortname="twitter"><img src="http://svgporn.com/logos/twitter.svg" alt="Twitter" class="twitter"></a>
+	        </div>
+	   		</div>
+
+	</div>
+</footer>
