cac00036971f4d70297691b6ca5e2369777b09bb
fullgod <fullgod@gmail.com>
2016-11-08 16:13:31 +0300

    design

--- template_parts/content-footer-ui.php
+++ template_parts/content-footer-ui.php
@@ -1,74 +1,71 @@
-<!--<section class="vc_row-fluid use">
+<section class="vc_row-fluid use">
+
+<div class="container">
 	<div class="row">
-	<div class="col-sm-6">
 		<div class="use_text">
-
+		<h2>Результат</h2>
 		<h3>Все разрабатываемые сайты, полностью готовы к работе, и не требуют от пользователя никаких дополнительных доработок и настроек.</h3>
-		</div>
-	</div>
 
-		<div class="vc_col-sm-1">
+	
+
+	
 			<div class="feature feature-icon-large">
 				<a href="http://getbootstrap.com/" target="_blank" class="feature feature-icon-large" data-shortname="bootstrap">
 				<img src="http://svgporn.com/logos/bootstrap.svg" alt="Bootstrap" class="bootstrap">
 				</a>
 			</div>
-		</div>
-		<div class="vc_col-sm-1">
+
 			<div class="feature feature-icon-large">
 				<a href="http://www.w3.org/TR/CSS/" target="_blank" class="feature feature-icon-large" data-shortname="css-3">
 				<img src="http://svgporn.com/logos/css-3.svg" alt="CSS3" class="css-3">
 				</a>
 			</div>
-		</div> 
-		<div class="vc_col-sm-1">
+
 			<div class="feature feature-icon-large">
 				<a href="http://sass-lang.com/" target="_blank" class="feature feature-icon-large" data-shortname="sass">
 				<img src="http://svgporn.com/logos/sass.svg" alt="Sass" class="sass"></a>
 			</div>
-		</div> 
-		<div class="vc_col-sm-1">
+
 			<div class="feature feature-icon-large">
 				<a href="http://www.w3.org/TR/html5/" target="_blank" class="feature feature-icon-large" data-shortname="html-5">
 				<img src="http://svgporn.com/logos/html-5.svg" alt="HTML5" class="html-5"></a>
 			</div>
-		</div>  
-		<div class="vc_col-sm-1">
+
 			<div class="feature feature-icon-large">
 				<a href="https://wordpress.org/" target="_blank" class="feature feature-icon-large" data-shortname="wordpress">
 				<img src="http://svgporn.com/logos/wordpress-icon.svg" alt="Wordpress" class="wordpress"></a>
 			</div>
-		</div> 
-		<div class="vc_col-sm-1">
+
 			<div class="feature feature-icon-large">
 				<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" class="feature feature-icon-large" data-shortname="javascript">
 				<img src="http://svgporn.com/logos/javascript.svg" alt="Javascript (JS)" class="javascript"></a>
 			</div>
-		</div> 
-
-  	</div>
+		</div>
+	</div>
+</div>
 
 </section>
--->
+
 <footer class="footer__ui">
 	<div class="container">
 		<div class="row">
 
-			<div class="col-sm-12">           
-				<a class="footer__ui-logo" title="UI" rel="home">Hacking UI</a>
+			<div class="col-sm-12">  
+				<span class="h-label">Теперь:</span> 
+				<h2>Связаться!</h2>
+				<p class="info">Если вы хотите поговорить о проекте, который вы хотите реализовать или нужен совет в ваших идеях, просто напишите мне сообщение. Я в настоящее время доступен для небольших проектов.</p>
+
+				<a class="button button--big" href="mailto:me@janlosert.com">Написать письмо</a>
+				<div class="social">
+                    <ul>
+                        <li class="item-left"><a href="https://dribbble.com/janlosert" onclick="track('ClickOut','footer-dribbble')" target="_blank">Dribbble</a></li>
+                        <li class="item-left"><a href="https://www.behance.net/janlosert" onclick="track('ClickOut','footer-behance')" target="_blank">Behance</a></li>
+                        <li class="item-left"><a href="https://twitter.com/JanLosert" onclick="track('ClickOut','footer-twitter')" target="_blank">Twitter</a></li>
+                        <li class="item-left"><a href="https://www.instagram.com/janlosert/" onclick="track('ClickOut','footer-instagram')" target="_blank">Instagram</a></li>
+                    </ul>
+                </div>
 			</div>
 
-		    <div class="col-sm-4 col-xs-12">
-        		<div class="footer__ui-copyright alignleft">© ui0
-        		</div>
-    		</div>
-	   	 	
-	   	 	<div class="col-sm-8 col-xs-12">
-	        	<div class="footer__ui-icons alignright">
-					<a href="https://www.linkedin.com/" target="_blank" class="feature" data-shortname="linkedin"><img src="http://svgporn.com/logos/linkedin.svg" alt="LinkedIn" class="linkedin"></a>
-					<a href="https://twitter.com/" target="_blank" class="feature" data-shortname="twitter"><img src="http://svgporn.com/logos/twitter.svg" alt="Twitter" class="twitter"></a>
-	        </div>
-	   		</div>
 
 	</div><!--end of container-->
 </footer>
