a8245a21774a83c3be9218301f3b8227d2d4e69e
fullgod <fullgod@gmail.com>
2016-11-24 16:29:29 +0300

    111

--- template_parts/content-header-portfolio.php
+++ template_parts/content-header-portfolio.php
@@ -1,44 +1,36 @@
-<!--header start-->
-
-
 <header class="page-header__menu">
-
-                <div class="container">
-                    <div class="logo">
-                        <a href="/">UI</a>
-                    </div>
-					<nav class="nav">
-					<div class="nav-collapse">
-												<ul>
-													<li><a href="/" class="link">Home</a>
-													</li>
-													<li><a href="case_studies.html" class="link">Case studies</a>
-													</li>
-													
-												</ul>
-					</div>
-    			<a href="#" class="contact">
-            <i class="icon"></i><span>Contact us</span></a>
-             </nav>
-        </div>
-
-	
-</header>
-
-
-
-<!--header end-->
+  <div class="container">
+    <div class="logo">
+      <a href="/">UI</a>
+    </div>
+    <nav class="nav">
+      <div class="nav-collapse">
+        <ul>
+          <li>
+            <a class="link" href="/">Home</a>
+          </li>
+          <li>
+            <a class="link" href="case_studies.html">Case studies</a>
+          </li>
+        </ul>
+      </div>
+      <a href="#" class="contact">
+              <i class="icon"></i><span>Contact us</span>
+      </a>
+    </nav>
+  </div>
+</header><!--header end-->
 <section class="page-header__portfolio">
-	<div class="container_fluid">
-		<div class="row">
-			<div class="col-md-12">
-            	<div class="page-header__portfolio__our_works">
-                	<div class="middle_text">
-                	<?php if (function_exists('ui_breadcrumbs')) ui_breadcrumbs(); ?>
-                    <h1 class="text-center"><?php echo get_the_title(); ?></h1>
-                </div>
-            	</div>
-        	</div>
-		</div>
-	</div>	
+  <div class="container_fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-header__portfolio__our_works">
+          <div class="middle_text">
+            <?php if (function_exists('ui_breadcrumbs')) ui_breadcrumbs(); ?>
+            <h1 class="text-center"><?php echo get_the_title(); ?></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
